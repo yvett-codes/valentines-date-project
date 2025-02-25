497798b6591dd3259057d6cce3f485ecf1497297
yvett-codes <yvett.nez@gmail.com>
2025-02-25 15:26:42 -0600

    deleted example button and added functionality to support changing button text

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-function myFunction() {
-    console.log("This button was clicked");
-}
 function startDate() {
     console.log("Let's go on a date!");
+    const element = document.getElementById("dateButton");
+    element.style.backgroundColor = "green";
+    element.textContent = "EEK";
 }
+
+const buttonMessages = [
+    "💗 Let's go! 💗",
+    "Strawberry Fields",
+    "Cake Outfit",
+    "Girl, you are the strawberry of my eye.",
+    "Pet the puppy",
+    "Strawberry Dessert"
+]
