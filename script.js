13f03f911d0a7a536ca033ab26b35825b6cb6dea
yvett-codes <yvett.nez@gmail.com>
2025-02-25 16:03:24 -0600

    added conditional logic to buttons for displaying choices

--- script.js
+++ script.js
@@ -4,17 +4,24 @@ function startDate() {
     const button = document.getElementById("dateButton");
     // button.addEventListener('click', doSomething);
     clickCounter++;
-    button.textContent = buttonMessages[clickCounter];
+    // how to reset textcontent to original?
+    if (clickCounter < buttonChoices.length) {
+        button.textContent = buttonChoices[clickCounter];
+    } else {
+        // Actually, I should be restarting the entire page I think
+        button.textContent = "STOOOOP";
+    }
 }
 
 function doSomething() {
     alert("Hello, world!")
 }
 
-const buttonMessages = [
+const buttonChoices = [
     "Strawberry Fields",
     "Cake Outfit",
     "Girl, you are the strawberry of my eye.",
     "Pet the puppy",
-    "Strawberry Dessert"
+    "Strawberry Dessert",
+    "Thanks for the date!"
 ]
