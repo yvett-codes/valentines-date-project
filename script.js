1237cc5cd1b9f99d68794ecff290dddadc6d0456
yvett-codes <yvett.nez@gmail.com>
2025-02-26 16:20:40 -0600

    added some styling and images to revolve with questions

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ let clickCounter = 0;
 function startDate() {
     const button = document.getElementById("dateButton");
     const message = document.getElementById("dateMessage");
+    const image = document.getElementById("mainImage");
     // button.addEventListener('click', doSomething);
     clickCounter++;
     if (clickCounter < buttonChoices.length) {
         button.textContent = buttonChoices[clickCounter];
         message.textContent = dateMessages[clickCounter]
+        image.src=dateImages[clickCounter]
     } else {
         location.reload();
     }
@@ -23,7 +25,6 @@ const buttonChoices = [
     "Girl, you are the strawberry of my eye.",
     "Pet the puppy",
     "Strawberry Dessert",
-    "Thanks for the date!",
     "🍓 PLAY AGAIN 🍓"
 ]
 
@@ -33,5 +34,14 @@ const dateMessages = [
     "Drop your best pickup line",
     "There's a puppy, what do you do?",
     "Time to wrap it up, what are you going to eat?",
-    "Miss Shortcake loved her date with you!"
+    "Miss Shortcake loved her date with you! Thanks for playing <3",
+]
+
+const dateImages = [
+    "https://media.giphy.com/media/3oz8xWxVOhxLMso3vO/giphy.gif?cid=790b7611x8052av7q7uepqdxc0ugi4wtwhpqyp73ltbrbqvp&ep=v1_gifs_search&rid=giphy.gif&ct=g",
+    "https://media.giphy.com/media/ZKcCTucRwOIz6/giphy.gif?cid=790b7611x8052av7q7uepqdxc0ugi4wtwhpqyp73ltbrbqvp&ep=v1_gifs_search&rid=giphy.gif&ct=g",
+    "https://media.giphy.com/media/Z2P9X3Upuw0ZexxDOr/giphy.gif?cid=790b7611x8052av7q7uepqdxc0ugi4wtwhpqyp73ltbrbqvp&ep=v1_gifs_search&rid=giphy.gif&ct=g",
+    "https://media.giphy.com/media/HQpDAiBdJM3bE4kOes/giphy.gif?cid=ecf05e47j0szduw1oanj03mrz3mug7c2m4netomkmfcx1th4&ep=v1_gifs_search&rid=giphy.gif&ct=g",
+    "https://media.giphy.com/media/B6Rtjz90dG97enfVbW/giphy.gif?cid=790b7611x8052av7q7uepqdxc0ugi4wtwhpqyp73ltbrbqvp&ep=v1_gifs_search&rid=giphy.gif&ct=g",
+    "https://media.giphy.com/media/aJb3WkzdjwTbNZ57BC/giphy.gif?cid=790b7611x8052av7q7uepqdxc0ugi4wtwhpqyp73ltbrbqvp&ep=v1_gifs_search&rid=giphy.gif&ct=g"
 ]
