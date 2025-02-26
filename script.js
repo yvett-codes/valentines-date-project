37eacf05d9662074543d0f58ba112a8a0d279eb2
yvett-codes <yvett.nez@gmail.com>
2025-02-26 15:43:13 -0600

    added id to messages and multiple messages array, added functionality for the page to refresh to play the game again

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ let clickCounter = 0;
 
 function startDate() {
     const button = document.getElementById("dateButton");
+    const message = document.getElementById("dateMessage");
     // button.addEventListener('click', doSomething);
     clickCounter++;
-    // how to reset textcontent to original?
     if (clickCounter < buttonChoices.length) {
         button.textContent = buttonChoices[clickCounter];
+        message.textContent = dateMessages[clickCounter]
     } else {
-        // Actually, I should be restarting the entire page I think
-        button.textContent = "STOOOOP";
+        location.reload();
     }
 }
 
@@ -23,5 +23,15 @@ const buttonChoices = [
     "Girl, you are the strawberry of my eye.",
     "Pet the puppy",
     "Strawberry Dessert",
-    "Thanks for the date!"
+    "Thanks for the date!",
+    "🍓 PLAY AGAIN 🍓"
+]
+
+const dateMessages = [
+    "Where are you taking Miss Shortcake?",
+    "What are you wearing?",
+    "Drop your best pickup line",
+    "There's a puppy, what do you do?",
+    "Time to wrap it up, what are you going to eat?",
+    "Miss Shortcake loved her date with you!"
 ]
